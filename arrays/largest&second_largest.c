74c529933e730a5e931f8ecc2c36b7876c2d7014
abhitsharma <54718347+abhitsharma@users.noreply.github.com>
2023-02-27 22:25:38 +0530

    Update largest&second_largest.c

--- arrays/largest&second_largest.c
+++ arrays/largest&second_largest.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    int arr[50], size, i;
+    int m1, m2;
+    printf("Enter size of the array: ");
+    scanf("%d", &size);
+
+    printf("Enter elements in the array: ");
+    for(i=0; i<size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    m1 = m2 = arr[50];
+    for(i=0; i<size; i++)
+    {
+        if(arr[i] > m1)
+        {
+            m2 = m1;
+            m1 = arr[i];
+        }
+        else if(arr[i] > m2 && arr[i] < m1)
+        {
+            
+            m2 = arr[i];
+        }
+    }
+
+    printf("First largest = %d\n", m1);
+    printf("Second largest = %d", m2);
+    return 0;
+}
 
