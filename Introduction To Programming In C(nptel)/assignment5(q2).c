d8c52df1f5c77a7f002b08a2b4253f3ef42aa7a7
abhitsharma <54718347+abhitsharma@users.noreply.github.com>
2023-02-27 20:46:14 +0530

    Update assignment5(q2).c

--- Introduction To Programming In C(nptel)/assignment5(q2).c
+++ Introduction To Programming In C(nptel)/assignment5(q2).c
@@ -1,1 +1,21 @@
 
+#include <stdio.h>
+
+int collatz(int n, int count) {
+    if (n == 1) {
+        return count;
+    } else if (n % 2 == 0) {
+        return collatz(n / 2, count + 1);
+    } else {
+        return collatz(3 * n + 1, count + 1);
+    }
+}
+
+int main() {
+    int n, count = 0;
+    
+    scanf("%d", &n);
+    count = collatz(n, count);
+    printf("%d",count);
+    return 0;
+}
