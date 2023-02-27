2e8d77c16f861ee6c51f60662db110055d8a9e40
abhitsharma <54718347+abhitsharma@users.noreply.github.com>
2023-02-27 14:09:31 +0530

    Create assignment1(q1).c
    You have a certain number of 100 rupee notes, 10 rupee notes and 1 rupee notes with you.
    There is an item you want to buy whose price is given to you.
    Write a program to find if the item is affordable, that is the price of the item is less than or equal to the current money you have.

---
+++ Introduction To Programming In C(nptel)/assignment1(q1).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void  main(){
+int a,b,c,d;
+int t;
+scanf("%d",&a);
+scanf("%d",&b);
+scanf("%d",&c);
+
+scanf("%d",&d);
+  t=(a*100)+(b*10)+c;
+  if (t>=d)
+  {
+  printf("1");
+  }
+  else{
+ printf("0");
+  }
+  
+}
