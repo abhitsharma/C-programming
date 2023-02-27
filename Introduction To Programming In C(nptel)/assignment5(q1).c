b0d4dae24fbce2522b0719b9d18ea021ecff98a9
abhitsharma <54718347+abhitsharma@users.noreply.github.com>
2023-02-27 20:38:36 +0530

    Update assignment5(q1).c

--- Introduction To Programming In C(nptel)/assignment5(q1).c
+++ Introduction To Programming In C(nptel)/assignment5(q1).c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+
+void reverse_string() {
+    char c;
+    if ((c = getchar()) != EOF) {
+        reverse_string();
+        putchar(c);
+    }
+}
+
+int main() {
+   
+    reverse_string();
+  
+    return 0;
+}
 
