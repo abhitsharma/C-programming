e6057c7ee769c88f588efc6b6fda767b371c091b
abhitsharma <54718347+abhitsharma@users.noreply.github.com>
2023-03-07 00:25:13 +0530

    Update assignment6(q1).c
    We say that a string 's' is an anagram of another string 't' if the letters in 's' can be rearranged to form 't'.
    For example, "butterfly" is an anagram of "flutterby", since a rearrangement of the first word results in the second.
    We say that a position 'i' in 's' and 't' match, if 's' is an anagram of 't', and s[i]==t[i].
    In this question, you will be given two words, 's' and 't'. You have to output the number of matching positions if s is an anagram of t, and -1 if s is not an anagram of t.

--- Introduction To Programming In C(nptel)/assignment6(q1).c
+++ Introduction To Programming In C(nptel)/assignment6(q1).c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LEN 100
+
+int main()
+{
+    char s[MAX_LEN + 1], t[MAX_LEN + 1];
+    int count[26] = {0}, i, match_count = 0;
+    scanf("%s %s", s, t);
+    
+    if (strlen(s) != strlen(t)) { // If the lengths are not equal, they cannot be anagrams
+        printf("-1");
+        return 0;
+    }
+
+    // Count the frequency of characters in s
+    for (i = 0; s[i] != '\0'; i++) {
+        count[s[i] - 'a']++;
+    }
+
+    // Check if each character in t matches with s and decrease the count accordingly
+    for (i = 0; t[i] != '\0'; i++) {
+        if (count[t[i] - 'a'] == 0) { // If the character is not present in s, they cannot be anagrams
+            printf("-1");
+            return 0;
+        }
+        count[t[i] - 'a']--;
+        if (s[i] == t[i]) { // If the characters match, increase the match count
+            match_count++;
+        }
+    }
+
+    printf("%d", match_count);
+    return 0;
+}
 
