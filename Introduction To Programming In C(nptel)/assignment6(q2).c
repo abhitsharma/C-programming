f065096cd999296bf61f89f8ad8477e7c7611aef
abhitsharma <54718347+abhitsharma@users.noreply.github.com>
2023-03-07 00:26:06 +0530

    Update assignment6(q2).c
    In a string, a "run" is a substring with consisting of consecutive occurrences of the same character.For example, the string "mississippi" contains the following runs - "ss", "ss" and "pp".
    In this question, given a string, you have to output the length of the longest run in the string.

--- Introduction To Programming In C(nptel)/assignment6(q2).c
+++ Introduction To Programming In C(nptel)/assignment6(q2).c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main() {
+    char s[101];
+    scanf("%s", s);
+    int max_run = 1;
+    int curr_run = 1;
+    for (int i = 1; s[i] != '\0'; i++) {
+        if (s[i] == s[i-1]) {
+            curr_run++;
+        } else {
+            if (curr_run > max_run) {
+                max_run = curr_run;
+            }
+            curr_run = 1;
+        }
+    }
+    if (curr_run > max_run) {
+        max_run = curr_run;
+    }
+    printf("%d", max_run);
+    return 0;
+}
