5564f491c2dc7e493298c9e8b5c082fb44c052ba
abhitsharma <54718347+abhitsharma@users.noreply.github.com>
2023-03-07 00:27:38 +0530

    Update assignment6(q3).c
    Given an n×ninteger Matrix A and an positive number ℓ such that 2ℓ+1≤n print the ℓ window smoothing of A. To get the  ℓ-window smoothing of A , we replace A[i][j]
     with the sum of the values of the 2ℓ+1×2ℓ+1 submatrix of A with centre at A[i][j].
    More precisely, the smoothed matrix B[i,j]=∑u=ilih∑v=jljhA[u][v] 
    where il=max(i−ℓ,0),ih=min(i+ℓ,n−1),jl=max(j−ℓ,0),jh=min(j+ℓ,n−1).

--- Introduction To Programming In C(nptel)/assignment6(q3).c
+++ Introduction To Programming In C(nptel)/assignment6(q3).c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int n, l, A[100][100], B[100][100];
+    scanf("%d", &n);
+    scanf("%d", &l);
+
+    // Read matrix A
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    // Calculate the smoothed matrix B
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            int il = i - l < 0 ? 0 : i - l;
+            int ih = i + l >= n ? n - 1 : i + l;
+            int jl = j - l < 0 ? 0 : j - l;
+            int jh = j + l >= n ? n - 1 : j + l;
+
+            int sum = 0;
+            for(int u=il; u<=ih; u++) {
+                for(int v=jl; v<=jh; v++) {
+                    sum += A[u][v];
+                }
+            }
+
+            B[i][j] = sum;
+        }
+    }
+
+    // Print the smoothed matrix B
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            printf("%d ", B[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
